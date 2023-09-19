a2b170e82aa0ccb6aeaa66d04627fb4b95df4a57
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-09-19 14:27:31 +0200

    feat: tooltip & truncate & index

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/ui';
+export * from './lib/components/icons/icon'
+
+export * from './lib/components/buttons/button'
+
+export * from './lib/components/table'
+export * from './lib/components/menu'
