7df831a5ed637d45164974afe03e0869d00f5b60
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-10-14 11:58:48 +0200

    feat(interface): init OrganizationState

---
+++ libs/interfaces/src/lib/states/organizations.interface.ts
@@ -0,0 +1,4 @@
+import { OrganizationEntity } from "../domains/organization.entity";
+import { DefaultEntityState } from "./default-entity-state.interface";
+
+export interface OrganizationState extends DefaultEntityState<OrganizationEntity> {}
