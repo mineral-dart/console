6ee46c15d7d4096bb045c0f6eee571401ef082f0
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-10-14 11:59:03 +0200

    feat(libs): init `state/store`

---
+++ libs/state/store/src/lib/root.ts
@@ -0,0 +1,34 @@
+import { type PreloadedState, combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  organization
+} from '@console/domains/organization'
+
+export const organizationReducer = combineReducers({
+  organizations: organization
+})
+
+export const rootReducer = combineReducers({
+
+  organization: organizationReducer
+})
+
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+  })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
+
+export const initialRootState = (): RootState => ({
+  organization: {
+    organizations: []
+  }
+})
