6d444f12e9dd0177586c4d54824ac23343db981d
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-10-14 18:19:06 +0200

    feat(): fetch organization

--- libs/pages/layout/src/lib/feature/layout.tsx
+++ libs/pages/layout/src/lib/feature/layout.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren, useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { setCurrentOrganizationIdOnstorage, setCurrentProjectIdOnStorage } from "../utils"
 import LayoutPage from "../ui/layout-page"
 import { useAuth } from "@console/auth"
 import { updateUserState } from "@console/redux"
+import { fetchOrganization, selectAllOrganization } from '@console/domains/organization'
+import { type AppDispatch, type RootState } from '@console/state/store'
 
 export interface LayoutProps {
   topBar?: boolean
@@ -13,10 +15,19 @@ export interface LayoutProps {
 export function Layout ({ topBar, children }: PropsWithChildren<LayoutProps>) {
   const { organizationId = '', projectId = '', environmentId = '', versionId } = useParams()
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch<AppDispatch>()
 
   const { useMe } = useAuth()
   const { data: user } = useMe()
+
+  useEffect(() => {  
+    dispatch(fetchOrganization())
+      .unwrap()
+      .then((data) => {
+        console.log(data);
+        
+      })    
+  }, [dispatch])
 
   useEffect(() => {
     if (user) {
@@ -25,7 +36,7 @@ export function Layout ({ topBar, children }: PropsWithChildren<LayoutProps>) {
   }, [user])
 
   const clusters: any[] = []
-  const organizations: any[] = []
+  const organizations: any[] = useSelector(selectAllOrganization)
 
   const environment = {}
 
@@ -33,6 +44,10 @@ export function Layout ({ topBar, children }: PropsWithChildren<LayoutProps>) {
     setCurrentOrganizationIdOnstorage(organizationId)
     setCurrentProjectIdOnStorage(projectId)
   }, [projectId, organizationId])
+
+  useEffect(() => {
+    console.log(organizations);
+  }, [organizations])
 
   return (
     <LayoutPage 
