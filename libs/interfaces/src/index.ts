17b3804e1ebe26a1cdd666e8c2e49da23c5975c8
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-10-15 12:09:50 +0200

    feat(): new export

--- libs/interfaces/src/index.ts
+++ libs/interfaces/src/index.ts
@@ -7,4 +7,5 @@ export * from './lib/pagination.interface'
 export * from './lib/organization.interface'
 export * from './lib/response'
 
+export * from './lib/domains'
 export * from './lib/states'
--- libs/interfaces/src/lib/states/index.ts
+++ libs/interfaces/src/lib/states/index.ts
@@ -1,3 +1,4 @@
 export * from './root.interface'
 export * from './default-entity-state.interface'
 export * from './organizations.interface'
+export * from './projects.interface'
