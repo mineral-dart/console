a8fb1ecebd75a3e235309b25683a30a4c54c567b
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-05 21:31:25 +0100

    feat(): fetch environments

--- libs/pages/environments/src/lib/feature/page-general-feature.tsx
+++ libs/pages/environments/src/lib/feature/page-general-feature.tsx
@@ -1,14 +1,18 @@
 import { useParams } from "react-router";
 import { PageGeneral } from "../ui/page-general";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+import { useFetchEnvironments } from "@console/domains/environment";
 
 export function PageGeneralFeature () {
   const { organizationId = '', projectId = '' } = useParams()
 
+  const res = useFetchEnvironments(projectId)
+  const { data: environments } = res
+
   return (
     <>
       <PageGeneral 
-        environments={[]}
+        environments={environments ||  []}
         clusterAvailable={true}
         isLoading={false}
       />
