3fc26e91cf153f0b37efed53643c0b8ccd451023
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-05 21:31:35 +0100

    feat(): add table in page

--- libs/pages/environments/src/lib/ui/page-general.tsx
+++ libs/pages/environments/src/lib/ui/page-general.tsx
@@ -1,8 +1,10 @@
 import { IconAwesomeEnum } from "@console/enums"
-import { Button, ButtonSize, EmptyState, useModal } from "@console/ui"
+import { Button, ButtonSize, EmptyState, Table, useModal } from "@console/ui"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { CreateEnvironmentModalFeature } from '@console/shared'
+import { TableRowEnvironments } from "./table-row-environments"
+import { SERVICES_GENERAL_URL, SERVICES_URL } from "@console/routes"
 
 export interface PageGeneralProps {
   environments: any[]
@@ -15,7 +17,7 @@ export function PageGeneral ({ environments, isLoading, clusterAvailable }: Page
 
   const { openModal, closeModal } = useModal()
   const [data, setData] = useState<any[]>([])
-  const [filter, setFilter] = useState([])
+  const [filter, setFilter] = useState<any[]>([])
   const [loading, setLoading] = useState<boolean | undefined>(isLoading)
 
   useEffect(() => {
@@ -23,18 +25,73 @@ export function PageGeneral ({ environments, isLoading, clusterAvailable }: Page
     setLoading(isLoading)
   }, [environments, isLoading])
 
-  const tableHead = []
+  const tableHead = [
+    {
+      title: !loading ? `${data?.length} environment${data?.length && data.length > 1 ? 's' : ''}` : `0 environment`,
+      className: 'px-4 py-2',
+      filter: [
+        {
+          search: true,
+          title: 'Filter by status',
+          key: 'status.state',
+        },
+        {
+          title: 'Filter by provider',
+          key: 'cloud_provider.provider',
+        },
+      ],
+    },
+    {
+      title: 'Update',
+      className: 'px-4 text-right',
+      sort: {
+        key: 'updated_at',
+      },
+    },
+    {
+      title: 'Type',
+      className: 'px-4 py-2 border-b-neutral-200 border-l h-full',
+      filter: [
+        {
+          search: true,
+          title: 'Filter by environment type',
+          key: 'mode',
+        },
+      ],
+    },
+  ]
 
-  const columnsWidth = '30% 25% 40%'
+  const columnWidth = '30% 25% 40%'
 
   return (
     <>
      { environments.length ?
      
       (
-        <div>
-          Environnements
-        </div>
+        <Table
+          dataHead={tableHead}
+          data={environments}
+          setFilter={setFilter}
+          filter={filter}
+          setDataSort={setData}
+          className="mt-2 bg-white rounded-sm flex-grow overflow-y-auto min-h-0"
+          columnsWidth={columnWidth}
+          defaultSortingKey="name"
+        >
+          <>
+          { data.map((currentData) => (
+            <TableRowEnvironments 
+              key={currentData.id}
+              data={currentData}
+              filter={filter}
+              dataHead={tableHead}
+              link={`${SERVICES_URL(organizationId, projectId, currentData.id)}${SERVICES_GENERAL_URL}`}
+              columnsWidth={columnWidth}
+              isLoading={loading}
+            />
+          ))}
+          </>
+        </Table>
       ) : (
         !isLoading && (
           <EmptyState
