2cbf4e038e2612c17692dc06b1423c7d0c434f3b
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-11-05 16:19:15 +0100

    feat(): libs interface

--- libs/interfaces/src/index.ts
+++ libs/interfaces/src/index.ts
@@ -6,6 +6,7 @@ export * from './lib/auth'
 export * from './lib/pagination.interface'
 export * from './lib/organization.interface'
 export * from './lib/response'
+export * from './lib/environment.interface'
 
 export * from './lib/domains'
 export * from './lib/states'
---
+++ libs/interfaces/src/lib/environment.interface.ts
@@ -0,0 +1,9 @@
+export interface Environment {
+  id: string
+  name: string
+  project?: any
+  last_updated_by?: string
+  mode?: string
+  created_at: string
+  updated_at: string
+}
